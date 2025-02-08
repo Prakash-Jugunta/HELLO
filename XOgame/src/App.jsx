384f44f847fb1af6f83564f18fba96af527e64bf
Prakash-Jugunta <Prakashjugunta132005@gmail.com>
2025-02-08 09:30:34 +0530

    dynamic programming

--- XOgame/src/App.jsx
+++ XOgame/src/App.jsx
@@ -1,25 +1,36 @@
+import { useState } from 'react';
 
-function Square({ value }){
-  return <button className="square">{ value } </button>;
+function Square({value, onSquareClick}){
+
+  return <button className="square" onClick={onSquareClick}>{ value } </button>;
 }
 
 export default function Board(){
+  const [squares, setSquares]=useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares=squares.slice();
+    nextSquares[i]="X";
+    setSquares(nextSquares);
+    console.log(nextSquares);
+  }
+
   return (
     <>
     <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} onSquareClick={handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
     </div>
     <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
     </div>
     <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
     </div>
     </>
   )
